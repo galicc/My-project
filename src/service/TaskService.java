aa978f4f945e87e0aa1cfbde0f3c8b004c0dfab1
IvanG <ivan500galich@gmail.com>
2025-05-26 16:46:02 +0200

    Update and fix task crud operations

--- src/service/TaskService.java
+++ src/service/TaskService.java
@@ -10,11 +10,8 @@ public class TaskService {
 
     HashSet<Task> tasks = new HashSet<>();
 
-    public Task create(String title, String description, Date deadline){
-        if (title == null ||
-            description == null ||
-            deadline == null
-        ) return null;
+    public Task create(String title, String description, Date deadline) {
+        if (title == null || title.isEmpty()) return null;
 
         Task newTask = new Task(title, description, deadline);
 
@@ -23,44 +20,51 @@ public class TaskService {
         return newTask;
     }
 
-    public Task getById(UUID id){
+    public Task getById(UUID id) {
         if (id == null) return null;
 
-        for(Task task : tasks){
+        for (Task task : tasks) {
             if (task.getUuid().equals(id)) return task;
         }
 
         return null;
     }
 
-    public HashSet<Task> getAll(){
+    public HashSet<Task> getAll() {
         return tasks;
     }
 
-    public Task update(Task updatedTask){
-        if (updatedTask == null) return null;
-        if (updatedTask.getUuid() == null ||
-            updatedTask.getTitle() == null ||
-            updatedTask.getDescription() == null  ||
-            updatedTask.getDeadline() == null  ||
-            updatedTask.getStatus() == null
-        ) return null;
+    public Task update(Task updatedTask) {
+        if (updatedTask == null || updatedTask.getUuid() == null) return null;
 
-        Task task = getById(updatedTask.getUuid());
-        if (task == null) return null;
-
-        tasks.remove(task);
-        tasks.add(updatedTask);
+        for (Task task : tasks) {
+            if (task.getUuid().equals(updatedTask.getUuid())) {
+                if (!updatedTask.getTitle().equals(task.getTitle())) {
+                    task.setTitle(updatedTask.getTitle());
+                }
+                if (!updatedTask.getDescription().equals(task.getDescription())) {
+                    task.setDescription(updatedTask.getDescription());
+                }
+                if (!updatedTask.getDeadline().equals(task.getDeadline())) {
+                    task.setDeadline(updatedTask.getDeadline());
+                }
+                if (!updatedTask.getStatus().equals(task.getStatus())) {
+                    task.setStatus(updatedTask.getStatus());
+                }
+            }
+        }
 
         return updatedTask;
     }
 
-    public boolean delete(UUID id){
+    public Task delete(UUID id){
+        if (id == null) return null;
+
         Task task = getById(id);
-        if (task == null) return false;
+        if (task == null) return null;
 
         tasks.remove(task);
 
-        return true;
+        return task;
     }
 }
