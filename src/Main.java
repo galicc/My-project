67d2f65ab67db15d6dd14eca2539603be28f927c
IvanG <ivan500galich@gmail.com>
2025-05-26 10:44:34 +0200

    Create task model

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import model.Task;
+
+import java.util.Date;
+import java.util.UUID;
+
 public class Main {
     public static void main(String[] args) {
+
+        Date date = new Date(); // same date
+
+        System.out.println();
+
+        Task emptyTask = new Task();
+        emptyTask.setUuid(UUID.fromString("f5f753a5-2fe7-4661-9d15-b10cd7055c13"));
+        emptyTask.setTitle("Title");
+        emptyTask.setDescription("Description");
+        emptyTask.setDeadline(date);
+        System.out.println(emptyTask);
+        System.out.println(emptyTask.hashCode());
+
+        Task task = new Task("Title", "Description", date);
+        //task.setUuid(UUID.fromString("f5f753a5-2fe7-4661-9d15-b10cd7055c13"));
+        System.out.println(task);
+        System.out.println(task.hashCode());
+
+        System.out.println(emptyTask.equals(task));
 
     }
 }
---
+++ src/enumeration/Status.java
@@ -0,0 +1,10 @@
+package enumeration;
+
+public enum Status {
+
+    TODO,
+    IN_PROGRESS,
+    DONE,
+    ERROR
+
+}
